58e87cb8b6f50d6bb6b08175aa883235363f25f7
hta393939 <hta393939@gmail.com>
2025-11-19 19:35:47 +0900

    commit

---
+++ tool/maker/index.js
@@ -0,0 +1,20 @@
+
+class Misc {
+  initialize() {
+
+  }
+
+  make1() {
+    /** @type {HTMLCanvasElement} */
+    const canvas = document.getElementById('maincanvas');    
+    const c = canvas.getContext('2d');
+    {
+        
+    }
+  }
+
+}
+const misc = new Misc();
+globalThis.misc = misc;
+misc.initialize();
+
