93766eba644938faccc6b3444d9f58a33f427ca1
hta393939 <hta393939@gmail.com>
2025-11-30 11:28:07 +0900

    WIP: suit

--- tool/tex30/add.js
+++ tool/tex30/add.js
@@ -17,45 +17,87 @@ var drawAdded = (canvas) => {
 
   let x = 0;
   let y = 0;
+  let hx = q * 0.25;
   const _suit = (i) => {
     switch (i) {
-    case 0:
-      c.moveTo(x + q * 2, y + q);
-      c.lineTo(x + q * 3, y + q);
-      c.lineTo(x + q * 3, y + q * 3);
-      c.lineTo(x + q, y + q * 3);
+    case 0: // スペード
+      c.moveTo(x + q * 2, y + q); // 上
+      c.bezierCurveTo(x + q * 3, y + q * 1.5,
+        x + q * 3, y + q * 1.5,
+        x + q * 3, y + q * 2, // 右
+      );
+      c.bezierCurveTo(x + q * 3, y + q * 3,
+        x + q * 2, y + q * 3,
+        x + q * 2, y + q * 2, // 中
+      );
+      c.bezierCurveTo(x + q * 3, y + q * 3,
+        x + q, y + q * 3,
+        x + q, y + q * 2, // 左
+      );
+      c.bezierCurveTo(x + q * 3, y + q * 3,
+        x + q * 2, y + q,
+        x + q * 2, y + q, // 上
+      );
+
+      c.moveTo(x + q * 1.5, y + q * 3); // 左
+      c.bezierCurveTo(x + q, y + q,
+        x + q * 2, y + q * 2.5,
+        x + q * 2, y + q * 2,
+      ); // 中上
+      c.bezierCurveTo(x + q * 2, y + q * 2.5,
+        x + q * 2, y + q * 2,
+        x + q * 2.5, y + q * 3,
+      );
+      c.closePath();
       c.fillStyle = 'rgb(0, 0, 0)';
       break;
-    case 1:
-      c.moveTo(x + q * 2, y + q * 2);
-      c.bezierCurveTo(
+    case 1: // ハート
+      c.moveTo(x + q * 2, y + q * 1.75); // C
+      c.bezierCurveTo( // 右
         x + q * 2, y + q * 1,
         x + q * 3, y + q * 1,
         x + q * 3, y + q * 2);
-      c.bezierCurveTo(
-        x + q * 3, y + q * 2.5,
-        x + q * 3, y + q * 2.5,
+      c.bezierCurveTo( // 右下へ
+        x + q * 3, y + q * 2.25, // B
+        x + q * 3, y + q * 2.5, // A
         x + q * 2, y + q * 3);
-      c.lineTo(x + q, y + q * 2);
+      c.bezierCurveTo( // 中下の左
+        x + q * 1, y + q * 2.5, // A
+        x + q * 1, y + q * 2.25, // B
+        x + q, y + q * 2,
+      );
       c.bezierCurveTo(
         x + q, y + q * 1,
         x + q * 2, y + q * 1,
-        x + q * 2, y + q * 2,
+        x + q * 2, y + q * 1.75, // C
       );
       c.fillStyle = 'rgb(255, 0, 0)';
       break;
-    case 2:
+    case 2: // ダイヤ
       c.moveTo(x + q * 2, y + q);
       c.lineTo(x + q * 3, y + q * 2);
       c.lineTo(x + q * 2, y + q * 3);
       c.lineTo(x + q, y + q * 2);
       c.fillStyle = 'rgb(255, 0, 0)';
       break;
-    case 3:
-      c.moveTo(x + q, y + q);
-      c.lineTo(x + q * 3, y + q);
-      c.lineTo(x + q * 3, y + q * 3);
-      c.lineTo(x + q, y + q * 3);
+    case 3: // クラブ
+      c.ellipse(x + q * 2, y + q,
+        hx, hx, 0, 0, Math.PI * 2
+      );
+
+      c.moveTo(x + q * 3 + hx, y + q * 2);
+      c.ellipse(x + q * 3, y + q * 2,
+        hx, hx, 0, 0, Math.PI * 2
+      );
+
+      c.moveTo(x + q + hx, y + q * 2);
+      c.ellipse(x + q, y + q * 2,
+        hx, hx, 0, 0, Math.PI * 2
+      );
+      c.moveTo(x + q * 2.5, y + q * 3);
+      c.lineTo(x + q * 2, y + q * 2);
+      c.lineTo(x + q * 1.5, y + q * 3);
+      c.closePath();
       c.fillStyle = 'rgb(0, 0, 0)';
       break;
     default:
